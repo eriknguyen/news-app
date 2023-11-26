84d8f967b017373869aff267847a355cba83e8fa
Erik Nguyen <eriknguyen.dev@gmail.com>
2023-11-26 18:14:00 +0800

    feat: setup react-query

--- src/components/RootProvider.tsx
+++ src/components/RootProvider.tsx
@@ -1,13 +1,22 @@
 'use client'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import { TitleMapProvider } from '@/components/TitleEditorContext'
 import { ViewHistoryProvider } from '@/components/ViewHistoryContext'
 
+// Create a client
+const queryClient = new QueryClient()
+
 function RootProvider({ children }: { children: React.ReactNode }) {
   return (
-    <TitleMapProvider>
-      <ViewHistoryProvider>{children}</ViewHistoryProvider>
-    </TitleMapProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <TitleMapProvider>
+        <ViewHistoryProvider>{children}</ViewHistoryProvider>
+      </TitleMapProvider>
+    </QueryClientProvider>
   )
 }
 
